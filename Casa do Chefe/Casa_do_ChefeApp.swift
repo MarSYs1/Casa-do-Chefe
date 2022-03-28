85657157f466bef2a07c15151d8a9515e6418d2a
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-03-28 19:02:53 +0100

    Initial Commit

---
+++ Casa do Chefe/Casa_do_ChefeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Casa_do_ChefeApp.swift
+//  Casa do Chefe
+//
+//  Created by Mauro Sys One on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Casa_do_ChefeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Casa do Chefe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Casa do Chefe
+//
+//  Created by Mauro Sys One on 3/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
